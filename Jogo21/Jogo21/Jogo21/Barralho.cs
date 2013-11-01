79f4457712dddaf9bd39272b4ef8fc13e1532c8a
Altamir Benkenstein <altamir.benkenstein@outlook.com>
2013-11-01 18:48:51 -0200

    Criado Barralho

---
+++ Jogo21/Jogo21/Jogo21/Barralho.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Jogo21
+{
+    public class Barralho : JogoDeCartas
+    {
+
+
+
+        public void ordenar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void embaralhar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Carta darCartas()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string listarBaralho()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Jogo21/Jogo21/Jogo21/JogoDeCartas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jogo21
+{
+    interface JogoDeCartas
+    {
+        //ordenar(), embaralhar(), darCartas(), listarBaralho().
+        void ordenar();
+        void embaralhar();
+        Carta darCartas();
+        String listarBaralho();
+    }
+}
